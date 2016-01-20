92d287004571caab6a14e8f77e4f549e31c2c3df
ya <yashm@outlook.com>
2016-01-20 19:50:35 +0100

    fix bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,18 @@
 //
 //= require jquery
 //= require jquery_ujs
+
+//= require moment.min
+//= require bootstrap/affix
+//= require bootstrap/alert
+//= require bootstrap/button
+//= require bootstrap/carousel
+//= require bootstrap/collapse
+//= require bootstrap/dropdown
+//= require bootstrap/tab
+//= require bootstrap/transition
+//= require bootstrap/scrollspy
+//= require bootstrap/modal
+//= require bootstrap/tooltip
+//= require bootstrap/popover
 //= require_tree .
