0be286983c0dda872f0204ca981a26bbd6ed9948
ya <yashm@outlook.com>
2016-01-20 23:13:18 +0100

    user controller
    rails g controller users
    type in controller actions
    add routes
    add views manually (only index works)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+	
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user =  User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path, :notice => "User deleted"
+  end
+end
