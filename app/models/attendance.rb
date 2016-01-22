423af0f1d2aa2bd9eb32f611d66f6e6c8488930d
ya <yashm@outlook.com>
2016-01-22 23:57:55 +0100

    minor fixes

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,4 +2,5 @@ class Attendance < ActiveRecord::Base
   belongs_to :guest
   belongs_to :attendance_rate
   belongs_to :event
+  validates :guest_id, presence: true
 end
--- app/models/event_group.rb
+++ app/models/event_group.rb
@@ -2,7 +2,10 @@ class EventGroup < ActiveRecord::Base
   belongs_to :attr1
   belongs_to :attr2
   belongs_to :attr3
+  has_many :events
+  has_many :attendances, through: :events
   def to_s
   	name
   end
+  accepts_nested_attributes_for :events, reject_if: proc { |attributes| attributes ['user_id'].blank? }, allow_destroy: true
 end
