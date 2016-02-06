03ccbb57b292910b9c1b907dbc113f5a4fef5446
ya <yashm@outlook.com>
2016-02-06 23:33:23 +0100

    ux basic service views

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,13 @@
 class Service < ActiveRecord::Base
   belongs_to :service_name
   has_many :event_groups
+  def name
+  	event_quantity.to_s + service_name.to_s + event_length.to_s + gtype.to_s + totalprice.to_s 
+  end
   def to_s
-  	event_quantity.to_s + service_name.to_s + event_length.to_s
+    name
+  end
+  def totalprice
+  	event_quantity * gprice
   end
 end
