2c32970346378f977156a6018a5b45ab9c1d0e75
ya <yashm@outlook.com>
2016-02-11 22:04:52 +0100

    morris.js #3 tests

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
             sortField: 'text'
         });
     }
-
     $('#calendar').fullCalendar({
         lang: 'en',
         minTime: "09:00:00",
@@ -24,6 +23,20 @@ $(document).ready(function(){
       data: $('#attendances_chart').data('attendances'),
       xkey: 'created_at',
       ykeys: ['id'],
+      labels: ['Created at']
+    });
+    new Morris.Donut({
+      element: 'attendances_pie',
+      data: $('#attendances_pie').data('attendances'),
+      ykeys: ['attendance_rate'],
       labels: ['Series A']
+    });
+    new Morris.Donut({
+      element: 'donut-example',
+      data: [
+        {label: "Download Sales", value: 12},
+        {label: "In-Store Sales", value: 30},
+        {label: "Mail-Order Sales", value: 20}
+      ]
     });
 });
