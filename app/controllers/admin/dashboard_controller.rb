8caeaa3129720a9b49f02d4168ea40a206d5428b
ya <yashm@outlook.com>
2016-02-26 19:37:32 +0100

    namespace navbar #1

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < Admin::BaseController
 
   def index
   	@events = Event.all
+    @attendances = Attendance.all
+	@totalduepayment = @attendances.map(&:duepayment).sum
+    @totaldueexpence = @events.map(&:dueexpence).sum
   end
 end
