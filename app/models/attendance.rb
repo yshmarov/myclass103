460b2638e17d1db8fa34d7d9e7e0f70d969757f7
ya <yashm@outlook.com>
2016-02-29 18:10:13 +0100

    fix bug with event_id in nested resources

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,13 +2,10 @@ class Attendance < ActiveRecord::Base
   belongs_to :guest
   belongs_to :attendance_rate
   belongs_to :event
-  validates :guest_id, :attendance_rate_id, :event_id, presence: true
+  validates :guest_id, :attendance_rate_id, presence: true
   #scope :on_user_events, lamda{ |user| joins(:event).where("events.user_id = ?", user.id) }
 
   def duepayment
 	attendance_rate.rate*event.event_group.service.gprice
-  end
-  def user_id
-  	event.user_id
   end
 end
