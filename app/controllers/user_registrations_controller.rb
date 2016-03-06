386b3002e18142e1ddaec1069eb5e31c89b78c51
ya <yashm@outlook.com>
2016-03-06 15:29:45 +0100

    del user devise :registerable

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -1,2 +1,9 @@
 class UserRegistrationsController < Devise::RegistrationsController
+  def new
+    redirect_to root_path
+  end
+
+  def create
+    redirect_to root_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable
+
   has_many :events
   has_many :expences
   has_many :event_groups, through: :events
   has_many :attendances, through: :events
   has_many :services, through: :event_groups
+
+  before_save { self.email = email.downcase }
+  validates :email, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+
   def to_s
     self.email.split(/@/).first
   end
