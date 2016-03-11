fceddc4d2c55f61db1b53a797ca932b3a9fd68c2
ya <yashm@outlook.com>
2016-03-11 20:17:31 +0100

    can not del room if has associations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,21 @@
 class Room < ActiveRecord::Base
+  before_destroy :check_for_associations
   belongs_to :office
   has_many :events
   validates :name, :office_id, presence: true
   def to_s
     name
   end
+
+  def events?
+  	events.any?
+  end
+  private
+  def check_for_associations
+    if events?
+      errors[:base] << "cannot delete : events are still planned"
+      false
+    end
+  end
+
 end
