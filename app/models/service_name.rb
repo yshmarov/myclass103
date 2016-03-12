099701b58483c013a2f2e7f1393ab89a8a06a5d6
ya <yashm@outlook.com>
2016-03-12 01:13:14 +0100

    service_name ux

--- app/models/service_name.rb
+++ app/models/service_name.rb
@@ -1,7 +1,20 @@
 class ServiceName < ActiveRecord::Base
+  before_destroy :check_for_associations
   has_many :services
   validates :name, presence: true
   def to_s
 	name
   end
+
+  def services?
+  	services.any?
+  end
+  private
+  def check_for_associations
+    if services?
+      errors[:base] << "cannot delete : events are still planned"
+      false
+    end
+  end
+
 end
